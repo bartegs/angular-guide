b11b09b2c6900628c999fb6c5e6d305cba7a1342
Bartlomiej Sadecki <barteksadecki@gmail.com>
2022-06-27 16:09:31 +0200

    add child routes

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -18,10 +18,18 @@ export class ServerComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params['id']);
+    });
   }
 
   onReload() {
     this.router.navigate(['servers'], { relativeTo: this.route });
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
   }
 }
