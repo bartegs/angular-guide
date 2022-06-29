6f0b3d689b014e13dbd269fb629b90cb09948ed2
Bartlomiej Sadecki <barteksadecki@gmail.com>
2022-06-29 18:08:48 +0200

    Template forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from './user.service';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  userActivated = false;
-  constructor(private userService: UserService) {}
+export class AppComponent {
+  answer = '';
+  defaultPet = 'pet';
+  genders = ['male', 'female'];
 
-  ngOnInit() {
-    this.userService.activatedEmitter.subscribe(
-      (didActivate) => (this.userActivated = didActivate)
-    );
+  user = {
+    username: '',
+    mail: '',
+    secretQuestion: '',
+    secretAnswer: '',
+    gender: '',
+    submitted: false,
+  };
+
+  @ViewChild('f') myForm!: NgForm;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+
+    // REPLACE WHOLE OBJECT
+    // this.myForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    // });
+
+    // REPLACE ONLY SELECTED VALUES
+    this.myForm.form.patchValue({
+      username: suggestedName,
+    });
+    console.log(this.myForm.form);
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.myForm);
+
+    this.user = {
+      username: this.myForm.value.username,
+      mail: this.myForm.value.email,
+      secretQuestion: this.myForm.value.secret,
+      secretAnswer: this.myForm.value.questionAnswer,
+      gender: this.myForm.value.gender,
+      submitted: true,
+    };
+
+    this.myForm.reset();
   }
 }
